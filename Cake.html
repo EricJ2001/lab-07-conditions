a1c23200b93db88c3a1c707fa55ca89f0dc20ec3
EricJ2001 <70540195+EricJ2001@users.noreply.github.com>
2020-10-23 23:12:30 -0400

    Done

---
+++ Cake.html
@@ -0,0 +1,17 @@
+!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script type="text/javascript">
+      var question = prompt("Do you want cake? (yes/no)");
+      if (question === "yes") {
+          console.log("Have some cake!!!!!!!!!!");
+        } else {
+          console.log("NO CAKE FOR YOU!!!");
+        }
+    </script>
+  </body>
+</html>
---
+++ reminder.v02.py
@@ -0,0 +1,19 @@
+usertime=float(input("what hour is it?"(0-23) "))
+if usertime <= 5:
+    print("It's early. You should be sleeping!")
+elif usertime <= 7:
+    print("wake up, brew that coffee, get that mile run in")
+elif usertime <= 9:
+    print("Time to go to work.")
+elif usertime <= 12:
+    print("you should be working!")
+elif usertime <= 17:
+    print("Do you feel that afternoon lull?")
+elif usertime <= 19:
+    print("Time to hit the gym...")
+elif usertime <= 21:
+    print("Gotta eat that dinner.")
+elif usertime <=23:
+    print("Get that SLEEP. And rePEAT!")
+else:
+    print("you didn't type an acceptable value!"(0-23)"))
