5e85bcd5bb81ea33120d82a9185dea30d5513098
EricJ2001 <70540195+EricJ2001@users.noreply.github.com>
2020-10-23 23:36:11 -0400

    done

---
+++ flowchart.py
@@ -0,0 +1,27 @@
+DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script type="text/javascript">
+      var move = prompt("does it move? (yes/no)");
+      if (move == "yes") {
+        var should = prompt("Should it? (yes/no)");
+        if (should == "yes") {
+          console.log("no problem");
+        { else if (should == "no") {
+          console.log("then use duct tape!!!");
+        } eles {
+          console.log("Answer my question!You didn't type yes or no.");
+        }
+      } eles if (move == "no")
+        [our 2nd "then" clause here];
+      } eles {
+        console.log("Answer my question! You didn't type yes or no.");
+      }
+
+    </script>
+  </body>
+</html>
