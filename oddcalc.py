e1b878a859f15787e5e6469c272f8afe4aa660f3
EricJ2001 <70540195+EricJ2001@users.noreply.github.com>
2020-10-23 23:37:09 -0400

    done

---
+++ oddcalc.py
@@ -0,0 +1,20 @@
+!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script >
+      var calc = prompt("3 + + 3");
+      console.log("Multiple");
+      var calc = prompt("3 + + 3");
+      console.log("Divide");
+      var calc = ("3 + + 3");
+      console.log("modulo');
+
+          console.log("NO CAKE FOR YOU!!!");
+
+    </script>
+  </body>
+</html>
---
+++ spanish.py
@@ -0,0 +1,23 @@
+!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script type="text/javascript">
+      var  = prompt("Give me a word in English...");
+       if ("the user says cat");
+          console.log("gato")
+      var = prompt("give me a word in English...");
+          console.log("perro")
+      var = ("give me a word in English...");
+       if ("the use says dog");
+          console.log("caballo");
+       if ("the user says horse");
+      
+
+
+    </script>
+  </body>
+</html>
